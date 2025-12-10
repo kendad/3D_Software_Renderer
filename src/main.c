cb7d172c5d1f7690858b99d291131c598681b9ba
kendad <kaushikiswary@gmail.com>
2025-12-10 12:51:58 +0000

    added matrix math

--- src/main.c
+++ src/main.c
@@ -1,10 +1,16 @@
+#include "matrix.h"
 #include "vector.h"
 #include <stdio.h>
 
 int main() {
-  vec3_t someVector = vec3_new(0.1, 0.2, 1.9);
-  vec3_t otherVector = vec3_new(0.1, 0.2, 0.4);
-  vec3_t new_cross = vec3_cross(someVector, otherVector);
-  printf("%0.3f %0.3f %0.3f \n", new_cross.x, new_cross.y, new_cross.z);
+  mat4_t id1 = mat4_make_identity();
+  mat4_t id2 = mat4_make_identity();
+  mat4_t result = mat4_mul_mat4(id1, id2);
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; j++) {
+      printf("%0.3f ", result.data[i][j]);
+    }
+    puts("\n");
+  }
   return 0;
 }
---
+++ src/matrix.c
@@ -0,0 +1,36 @@
+#include "matrix.h"
+#include "vector.h"
+
+mat4_t mat4_make_identity() {
+  mat4_t matrix = {
+      .data = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}}};
+  return matrix;
+}
+
+vec4_t mat4_mul_vec4(mat4_t m, vec4_t v) {
+  vec4_t result = {.x = m.data[0][0] * v.x + m.data[0][1] * v.y +
+                        m.data[0][2] * v.z + m.data[0][3] * v.w,
+
+                   .y = m.data[1][0] * v.x + m.data[1][1] * v.y +
+                        m.data[1][2] * v.z + m.data[1][3] * v.w,
+
+                   .z = m.data[2][0] * v.x + m.data[2][1] * v.y +
+                        m.data[2][2] * v.z + m.data[2][3] * v.w,
+
+                   .w = m.data[3][0] * v.x + m.data[3][1] * v.y +
+                        m.data[3][2] * v.z + m.data[3][3] * v.w};
+  return result;
+}
+
+mat4_t mat4_mul_mat4(mat4_t a, mat4_t b) {
+  mat4_t m;
+
+  for (int i = 0; i < 4; ++i) {
+    for (int j = 0; j < 4; ++j) {
+      m.data[i][j] = a.data[i][0] * b.data[0][j] + a.data[i][1] * b.data[1][j] +
+                     b.data[i][2] * b.data[2][j] + b.data[i][3] * b.data[3][j];
+    };
+  }
+
+  return m;
+}
